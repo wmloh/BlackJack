08d5b0748755beead88eb3cede5ec3a4f06852b8
Wei Min Loh <32558880+wmloh@users.noreply.github.com>
2018-07-29 19:11:49 +0800

    removed data_struct

--- data_struct/data_struct.h
+++
@@ -1,9 +0,0 @@
-#include <stdbool.h>
-
-struct Deck *initDeck(void);
-
-char **getAllCards(struct Deck *deck);
-
-int *getCardsLeft(struct Deck *deck);
-
-char *getCard(struct Deck *deck, int suit, int num); 
